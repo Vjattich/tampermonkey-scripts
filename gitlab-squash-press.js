f160b5a8e34fde3a2823e94182375b9357104562
Vjattich <sektaaccel@gmail.com>
2025-12-30 15:32:58 +0500

    Create gitlab-squash-press.js

---
+++ gitlab-squash-press.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         squash press
+// @namespace    http://tampermonkey.net/
+// @version      2025-12-24
+// @description  press squash commit radio button on gitlab ui
+// @match        https://vcs.proitr.ru/dots-java/*/*/merge_requests/new?*
+// @author       Vjattich
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function pressSquash () {
+      const selector = 'merge_request_squash';
+      document.getElementById(selector).click();
+    }
+
+    pressSquash()
+
+})();
