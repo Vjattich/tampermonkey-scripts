92196d8404e912d0cfd101f4e6531bc7a3ad9332
Vjattich <sektaaccel@gmail.com>
2025-12-30 15:39:50 +0500

    Update gitlab-squash-press.js

--- gitlab-squash-press.js
+++ gitlab-squash-press.js
@@ -3,7 +3,6 @@
 // @namespace    http://tampermonkey.net/
 // @version      2025-12-24
 // @description  press squash commit radio button on gitlab ui
-// @match        https://vcs.proitr.ru/dots-java/*/*/merge_requests/new?*
 // @author       Vjattich
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
 // ==/UserScript==
