84a8966c8c315b1621bdf6260a7895b558fc5581
alukinykh <lukinykh_av@proitr.ru>
2026-04-12 11:32:05 +0500

    improve script

--- gitlab-copy-merge-info.js
+++ gitlab-copy-merge-info.js
@@ -1,33 +1,25 @@
 // ==UserScript==
-// @name         gitlab-copy-merge-info
+// @name         copy merged info
 // @namespace    http://tampermonkey.net/
 // @version      2026-04-07
-// @description  copy merged branch and task text info
+// @description  copy merged branch and task text
 // @author       Vjattich
 // @grant        GM_setClipboard
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    const matchJira = /[A-Z][A-Z0-9]+-\d+/;
 
-    function getMRData() {
-
-        const titleText = document.querySelector('[data-testid="title-content"]').innerText;
-        const jiraMatch = titleText.match(/[A-Z][A-Z0-9]+-\d+/);
-        const jiraKey = jiraMatch ? 'link' + jiraMatch[0] : 'Jira Key Not Found';
-
-        let targetBranch = 'unknown-branch';
-        const detailsRow = document.querySelector('.detail-page-description')
-        if (detailsRow) {
-            const ch = detailsRow.childNodes
-            const module = window.location.pathname.split('/')[2]
-            targetBranch = '- ' + module + ' с ветки ' + detailsRow.childNodes[ch.length-4].innerText;
-        }
-
-
-        return `Залил в БШ\n${jiraKey}\n${targetBranch}`;
+    function getMRData(jiraMatch, branch, module) {
+        const jiraKey = jiraMatch ? 'link' + jiraMatch : "Jira Key Not Found";
+        const scope = GM_getValue(jiraMatch);
+        return Object.values(scope).reduce(function (acc, s) { return acc +'\n' + s }, `Залил в БШ\n${jiraKey}`);
     }
 
     function createCopyButton() {
@@ -38,6 +30,39 @@
 
         if (!mergeButton) {
             return;
+        }
+
+        const detailsRow = document.querySelector('.detail-page-description')
+
+        let branch = null;
+        let branchFrom = null;
+
+        //its way to fast patch for me
+        if (detailsRow) {
+            branch = detailsRow.childNodes[detailsRow.childNodes.length-4].innerText;
+            branchFrom = detailsRow.childNodes[detailsRow.childNodes.length-8].innerText;
+            if (branch.indexOf('seamless') === -1) {
+                return;
+            }
+        }
+
+        const titleText = document.querySelector('[data-testid="title-content"]').innerText;
+        let jiraMatch = titleText.match(matchJira) || branchFrom.match(matchJira) || [];
+        jiraMatch = jiraMatch[0];
+
+        if (!jiraMatch) {
+            return;
+        }
+
+        const module = window.location.pathname.split('/')[2];
+        const isMerged = document.querySelector('[aria-label="Merged"]');
+
+        if (isMerged) {
+            let jiraObj = GM_getValue(jiraMatch) || {};
+            if (jiraObj && !jiraObj[module]) {
+                jiraObj[module] = '- ' + module + ' с ветки ' + branch;
+                GM_setValue(jiraMatch, jiraObj);
+            }
         }
 
         const targetContainer = mergeButton.parentNode;
@@ -52,7 +77,7 @@
 
             btn.onclick = () => {
 
-                let textToCopy = getMRData();
+                let textToCopy = getMRData(jiraMatch);
 
                 navigator.clipboard.writeText(textToCopy).then(() => {
                     const originalText = btn.innerText;
@@ -62,6 +87,7 @@
                     setTimeout(() => {
                         btn.innerText = originalText;
                         btn.classList.remove('btn-confirm');
+                        GM_deleteValue(jiraMatch);
                     }, 2000);
                 });
             };
